8aea44ae8f9b43975e9845baa0cdae137528cb1c
Jason Holt Smith <bicarbon8@gmail.com>
2021-05-04 17:27:53 +0100

    repo moved to https://github.com/bicarbon8/automated-functional-testing

--- src/index.ts
+++
@@ -1,9 +0,0 @@
-/* configuration */
-export * from './configuration/test-platform';
-/* facets */
-export * from './facets/abstract-facet';
-export * from './facets/ielement-options';
-/* sessions */
-export * from './sessions/abstract-session-generator-plugin';
-export * from './sessions/isession';
-export * from './sessions/session-generator-plugin-manager';
